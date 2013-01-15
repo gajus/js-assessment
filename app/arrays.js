8b2659bf6c281be4d30208ba7ad13c53b840df6a
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-15 22:20:07 +0000

    best practices completed.

--- app/arrays.js
+++ app/arrays.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+	    return arr.indexOf(item);
     },
 
     sum : function(arr) {
--- app/bestPractices.js
+++ app/bestPractices.js
@@ -6,31 +6,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
  * populated at all; your job is to fill them out.
  */
 define(function() {
-  return {
-    globals : function() {
-      myObject = {
-        name : 'Jory'
-      };
-
-      return myObject;
-    },
-
-    functions : function(flag) {
-      if (flag) {
-        function getValue() { return 'a'; }
-      } else {
-        function getValue() { return 'b'; }
-      }
-
-      return getValue();
-    },
-
-    parseInt : function(num) {
-      return parseInt(num);
-    },
-
-    identity : function(val1, val2) {
-
-    }
+	return {
+		globals: function () {
+			var myObj = {
+				name: 'Jory'
+			};
+			return myObj;
+		},
+		functions: function (flag) {
+			// or:
+			// var getValue ? function () { return 'a'; } : function () { return 'b'; } // for whatever reason you'd want that
+			return flag ? 'a' : 'b';
+		},
+		parseInt: function (num) {
+			return parseInt(num, 10);
+		},
+		identity: function (val1, val2) {
+			return val1 === val2;
+		}
   };
 });
