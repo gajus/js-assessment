8fa0eabc2955b79120fbd2aa14937fc318fc6800
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-16 00:56:39 +0000

    regex completed.

--- app/numbers.js
+++ app/numbers.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+	    return num.toString(2)[bit];
     },
 
     base10: function(str) {
--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+	    return /\d/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+	    return /([a-z])\1/i.test(str);
     },
 
     endsWithVowel : function(str) {
-
+	    return /[aeiuo]$/i.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+	    var match = /(\d{3})/.exec(str);
+	    return match === null ? false : match[0];
     },
 
     matchesPattern : function(str) {
-
+	    return /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(str);
     },
     isUSD : function(str) {
-
+	    return /^\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?$/.test(str);
     }
   };
 });
