fc146dd5898e8ee7c3f0f4cf65f63a9f4008d6d3
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-16 12:35:36 +0000

    Recursion.

--- app/recursion.js
+++ app/recursion.js
@@ -2,12 +2,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    listFiles: function(data, dirName) {
-
+    listFiles: function (data, dirName) {    	   	
+    	return (function self(data, includePath) {
+    		var fileNames = [];
+	    	
+	    	data.files.forEach(function (elm) {
+		    	if (typeof elm === 'string') {
+		    		if (!dirName || includePath) {
+			    		fileNames.push(elm);
+		    		}
+		    	} else {
+			    	fileNames = fileNames.concat(self(elm, includePath || elm.dir === dirName));
+		    	}
+	    	});
+	    	
+	    	return fileNames;
+    	}(data, data.dir === dirName));
     },
-
+    
     permute: function(arr) {
-
+		// ?
     }
   };
 });
