a4f36bbcc37ba89ed4f4fc6f7b5cafc85e81ddc2
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-16 13:05:25 +0000

    Numbers.

--- app/numbers.js
+++ app/numbers.js
@@ -3,20 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-	    return num.toString(2)[bit];
+	    // not familiar with binary opeprations
     },
 
     base10: function(str) {
-
+	    return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+		// not familiar with binary opeprations
     },
 
     multiply: function(a, b) {
-
-    }
+		var getMultiplier = function (a) {
+			var fraction = (a + '').split('.');
+			return fraction[1] ? Math.pow(10, fraction[1].length) : 1;
+		};
+		
+		var aAdjust = getMultiplier(a),
+			bAdjust = getMultiplier(b);
+		
+		return ((a * aAdjust) * (b * bAdjust)) / (aAdjust * bAdjust);
+	}
   };
 });
 
