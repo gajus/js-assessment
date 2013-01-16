9385750338579a54a6795c0fc6e1060e9731cd9c
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-16 01:10:16 +0000

    completed objects.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+	    return fn.apply(obj, [].slice.call(arguments));
     },
 
     alterObjects : function(constructor, greeting) {
-
+	    constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+	    var val,
+	    	result = [];
+	    for (val in obj) {
+		    if (obj.hasOwnProperty(val)) {
+			    result.push( val + ': ' + obj[val] );
+		    }
+	    }
+	    // or I could have used Object.getOwnPropertyNames
+	    return result;
     }
   };
 });
